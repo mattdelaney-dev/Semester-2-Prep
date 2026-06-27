50f4961631641210dee9e016e8d436e36d273cbc
Matthew Delaney <matt.delaney.dev@gmail.com>
2026-06-27 23:11:19 +1000

    Completed Variables, primitive and reference.

---
+++ java-oop/src/week1/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        // THIS IS MY FIRST JAVA PROGRAM
+
+        /*
+            This
+            is
+            a
+            multi-line comment.
+         */
+
+        System.out.println("Hello World!");
+        System.out.println("Hey Hey");
+        System.out.println("Hey Hey");
+    }
+}
--- java-oop/src/week1/Variables.java
+++ java-oop/src/week1/Variables.java
@@ -2,6 +2,41 @@
 // Covers: int, double, boolean, String, char
 public class Variables {
     public static void main(String[] args) {
-        // Add your code here as you follow along with Bro Code
+
+        // ❎ variable = A reusable container for a value.
+        //                          A variable behaves as if it was the value it contains.
+
+        // 🟥 Primitive = simple value stored directly in memory (stack)
+        // 🟦 Reference = memory address (stack) that points to the (heap)
+
+        // 🟥 Primitive vs 🟦 Reference
+        //    ---------                      ---------
+        //    int                           string
+        //    double                    array
+        //    char                        object
+        //    boolean
+
+        int age = 0;
+        int year = 2026;
+
+        double salary = 5.00;
+
+        char gender = 'M';
+        char grade = 'F';
+
+        boolean isStudent = false;
+
+        if(isStudent){
+            System.out.println("Student is a student");
+        }
+        else{
+            System.out.println("Student is not a student");
+        }
+
+        String name = "John";
+
+
+        System.out.println("Age: " + age);
+        System.out.print("The year is " + year);
     }
 }
