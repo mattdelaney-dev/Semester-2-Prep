224ea71594aa1f32b154acaca6efc13b4987f4dc
Matthew Delaney <matt.delaney.dev@gmail.com>
2026-06-27 23:38:33 +1000

    Finished First lesson of R studio

--- r-analytics/scripts/week1/basics.R
+++ r-analytics/scripts/week1/basics.R
@@ -1,6 +1,35 @@
 # Day 2 — R basics
 # Covers: variables, assignment, arithmetic
 # Resource: MarinStatsLectures videos 1-2
-
+►How to assign values to object is R using the equal sign (=) or less than sign with dash 
+►How to use ls function to see the data stored in R
+► How to use rm function to remove an object in R
+►How to add, subtract, multiply, and divide in R
+► How to use sqrt function in R to find the square root of an object 
+► How to use log function in R to calculate logarithms  
+► How to use exp function in R to take the exponent or anti-log 
+► How to use abs function in R to calculate the absolute value
+► and a few handy keyboard shortcuts in R 
 # Add your code here as you follow along
-
+> z = x-y
+> x*y
+[1] 180
+> x/y
+[1] 2.222222
+> y^2
+[1] 81
+> x^2
+[1] 400
+> sqrt(y)
+[1] 3
+> y^(-1/2)
+[1] 0.3333333
+> log(y)
+[1] 2.197225
+> exp(y)
+[1] 8103.084
+> log2(y)
+[1] 3.169925
+> abs(-14)
+[1] 14
+> # Using up arrow or down brings previous input
